cb107ee898f5c6d21901375790474a43f422e185
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2017-03-03 12:24:52 -0500

    Add mongoose and environment vars

--- starter-code/app.js
+++ starter-code/app.js
@@ -5,6 +5,13 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 const expressLayouts = require('express-ejs-layouts');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+
+dotenv.config();
+mongoose.connect(process.env.MONGODB_URI);
+
 
 var index = require('./routes/index');
 var users = require('./routes/users');
